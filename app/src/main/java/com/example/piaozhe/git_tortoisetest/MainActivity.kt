24633ead499f03c86a221d805a3eb5b0a5cb5acb
zhepEric <zhep1023@163.com>
2018-06-11 15:02:11 +0800

    添加Application类

--- app/src/main/java/com/example/piaozhe/git_tortoisetest/MainActivity.kt
+++ app/src/main/java/com/example/piaozhe/git_tortoisetest/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.piaozhe.git_tortoisetest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("TAG","hello world")
     }
 }
---
+++ app/src/main/java/com/example/piaozhe/git_tortoisetest/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.example.piaozhe.git_tortoisetest
+
+import android.app.Application
+import android.content.Context
+
+/**
+ * Created by piaozhe on 2018/6/11.
+ */
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+    }
+    
+}
