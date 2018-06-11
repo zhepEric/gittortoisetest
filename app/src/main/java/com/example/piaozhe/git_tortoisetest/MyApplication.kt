4415440f9a65d12ac038cb8f9cc00bc49da067af
zhepEric <zhep1023@163.com>
2018-06-11 15:06:25 +0800

    change log

--- app/src/main/java/com/example/piaozhe/git_tortoisetest/MyApplication.kt
+++ app/src/main/java/com/example/piaozhe/git_tortoisetest/MyApplication.kt
@@ -2,6 +2,7 @@ package com.example.piaozhe.git_tortoisetest
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 
 /**
  * Created by piaozhe on 2018/6/11.
@@ -9,10 +10,11 @@ import android.content.Context
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Log.i("hahah","what")
     }
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
     }
-    
+
 }
