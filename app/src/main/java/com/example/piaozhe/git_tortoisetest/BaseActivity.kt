197984789c4cee3f3c162c06b92bd72de03f5c4c
zhepEric <zhep1023@163.com>
2018-06-11 17:43:01 +0800

    添加baseactivity

--- app/src/main/java/com/example/piaozhe/git_tortoisetest/BaseActivity.kt
+++ app/src/main/java/com/example/piaozhe/git_tortoisetest/BaseActivity.kt
@@ -1,5 +1,29 @@
 package com.example.piaozhe.git_tortoisetest
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 /**
  * Created by piaozhe on 2018/6/11.
  */
+class BaseActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
