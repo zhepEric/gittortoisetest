6b035a09431fad97fd41a0338780aee6b4d43514
zhepEric <zhep1023@163.com>
2018-12-06 17:45:37 +0800

    修复release_v1.0.0中的Bug

--- app/src/main/java/com/example/piaozhe/git_tortoisetest/MainActivity.kt
+++ app/src/main/java/com/example/piaozhe/git_tortoisetest/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("TAG","hello world")
+        Log.i("TAG","fixed bug")
+
     }
 
     override fun onRestart() {
